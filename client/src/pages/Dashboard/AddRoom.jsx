298dc8ee0fab6952f0f45847d40ebb2f61152ed5
Waseque Arafat <waseque.ndc@gmail.com>
2024-07-05 12:51:51 +0600

    add room er kaj start

---
+++ client/src/pages/Dashboard/AddRoom.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AddRoomForm from "../../components/Forms/AddRoomForm";
+
+const Addroom = () => {
+  return <AddRoomForm> </AddRoomForm>;
+};
+
+export default Addroom;
